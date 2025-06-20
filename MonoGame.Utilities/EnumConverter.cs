5dd1c2917298d99309919808d71d6ee4a1834d6c
TechPizza <23627133+TechPizzaDev@users.noreply.github.com>
2025-06-20 19:08:53 +0200

    Use Unsafe.BitCast in EnumConverter

--- MonoGame.Utilities/EnumConverter.cs
+++ MonoGame.Utilities/EnumConverter.cs
@@ -1,56 +1,58 @@
 ﻿using System;
-using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
 
 namespace MonoGame.Framework
 {
     public static class EnumConverter
     {
         public static long ToInt64<TEnum>(TEnum value)
-            where TEnum : Enum
+            where TEnum : unmanaged, Enum
         {
-            return Helper<TEnum>.ConvertFrom(value);
+            return Unsafe.SizeOf<TEnum>() switch
+            {
+                sizeof(sbyte) => Unsafe.BitCast<TEnum, sbyte>(value),
+                sizeof(short) => Unsafe.BitCast<TEnum, short>(value),
+                sizeof(int) => Unsafe.BitCast<TEnum, int>(value),
+                _ => Unsafe.BitCast<TEnum, long>(value)
+            };
         }
 
         [CLSCompliant(false)]
         public static ulong ToUInt64<TEnum>(TEnum value)
-            where TEnum : Enum
+            where TEnum : unmanaged, Enum
         {
-            return (ulong)ToInt64(value);
+            return Unsafe.SizeOf<TEnum>() switch
+            {
+                sizeof(byte) => Unsafe.BitCast<TEnum, byte>(value),
+                sizeof(ushort) => Unsafe.BitCast<TEnum, ushort>(value),
+                sizeof(uint) => Unsafe.BitCast<TEnum, uint>(value),
+                _ => Unsafe.BitCast<TEnum, ulong>(value)
+            };
         }
-
+        
         public static TEnum ToEnum<TEnum>(long value)
-            where TEnum : Enum
+            where TEnum : unmanaged, Enum
         {
-            return Helper<TEnum>.ConvertTo(value);
+            return Unsafe.SizeOf<TEnum>() switch
+            {
+                sizeof(sbyte) => Unsafe.BitCast<sbyte, TEnum>((sbyte) value),
+                sizeof(short) => Unsafe.BitCast<short, TEnum>((short) value),
+                sizeof(int) => Unsafe.BitCast<int, TEnum>((int) value),
+                _ => Unsafe.BitCast<long, TEnum>(value)
+            };
         }
 
         [CLSCompliant(false)]
         public static TEnum ToEnum<TEnum>(ulong value)
-            where TEnum : Enum
+            where TEnum : unmanaged, Enum
         {
-            return ToEnum<TEnum>((long)value);
-        }
-
-        private static class Helper<TEnum>
-        {
-            public static Func<TEnum, long> ConvertFrom { get; } = GenerateFromConverter();
-            public static Func<long, TEnum> ConvertTo { get; } = GenerateToConverter();
-
-            private static Func<TEnum, long> GenerateFromConverter()
+            return Unsafe.SizeOf<TEnum>() switch
             {
-                var parameter = Expression.Parameter(typeof(TEnum));
-                var conversion = Expression.Convert(parameter, typeof(long));
-                var method = Expression.Lambda<Func<TEnum, long>>(conversion, parameter);
-                return method.Compile();
-            }
-
-            private static Func<long, TEnum> GenerateToConverter()
-            {
-                var parameter = Expression.Parameter(typeof(long));
-                var conversion = Expression.Convert(parameter, typeof(TEnum));
-                var method = Expression.Lambda<Func<long, TEnum>>(conversion, parameter);
-                return method.Compile();
-            }
+                sizeof(byte) => Unsafe.BitCast<byte, TEnum>((byte) value),
+                sizeof(ushort) => Unsafe.BitCast<ushort, TEnum>((ushort) value),
+                sizeof(uint) => Unsafe.BitCast<uint, TEnum>((uint) value),
+                _ => Unsafe.BitCast<ulong, TEnum>(value)
+            };
         }
     }
 }
